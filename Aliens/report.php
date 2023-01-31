3c1976bbae8beeb51574842f027354c16f3c1204
TUD-RSmith <71321440+TUD-RSmith@users.noreply.github.com>
2023-01-31 16:49:17 +0000

    added comments to report.php

--- Aliens/report.php
+++ Aliens/report.php
@@ -11,13 +11,16 @@
 <?php
 $when_it_happened = $_GET['whenithappened'];
 $first_name = $_GET['firstname'];
+//create variables for the rest of the form fields here**
 
 echo 'hi ' . $first_name . '<br/>';
 echo 'Thanks for submitting the form. <br />';
 echo 'You said it happened ' . $when_it_happened;
+//note the variable contents is output in these strings using the echo function.
+//Also note that HTML tags are treated as strings here too because they are inside the php environment
 
+//the php environment is closed with the question-mark, greater-than symbol sequence below this comment
 ?>
-
 
 
 
